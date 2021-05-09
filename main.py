1c99972962258706168fbc660ea29ea9f3eedff0
koouu <38750134+koouu@users.noreply.github.com>
2021-05-09 09:41:52 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,53 @@
-# coding: utf-8
-from flask import Flask, render_template
-from flask import request
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+#from werkzeug import secure_filename
 
-app = Flask(__name__) 
+from UGATIT import UGATIT
+from utils import *
 
-@app.route("/")
-def index():
-    return "Hello Flask!"
+import torch
+import numpy as np
+import cv2
+#from image_process import canny
+from datetime import datetime
+import os
+import string
+import random
 
-@app.route("/login") 
-def login():
-    return render_template("login.html")
 
-@app.route("/login_manager", methods=["POST"])  #追加
-def login_manager():
-    return "ようこそ、" + request.form["username"] + "さん"
+SAVE_DIR = "./images"
 
-if __name__ == "__main__":
-    # webサーバー立ち上げ
-    app.run()
+
+
+app = Flask(__name__)
+
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploader_file():
+    
+   gan=UGATIT()
+   gan.build_model()  
+   if request.method == 'POST':
+      stream = request.files['file'].stream
+      img_array = np.asarray(bytearray(stream.read()), dtype=np.uint8)
+      img = cv2.imdecode(img_array, 1)
+      save_path = os.path.join(SAVE_DIR,"aa.jpg")
+      cv2.imwrite(save_path, img)
+      #tt=torch.as_tensor(img)
+      #print(type(tt))
+      #cv2.imwrite(os.path.join('Base_%d.png' % (10 + 1)), RGB2BGR(tensor2numpy(denorm(tt))) * 255.0)
+      gan.test()
+      
+      
+      f = request.files['file']
+      #f.save(secure_filename(f.filename))
+      return render_template("ch.html", user_image = f.filename)
+		
+if __name__ == '__main__':
+    
+      
+
+   app.run(debug = True)
---
+++ t.py
@@ -0,0 +1,5 @@
+import torch
+
+print(torch.cuda.device_count())
+print(torch.cuda.device_count())
+# 1.7.1
